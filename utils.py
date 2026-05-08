3665ba4f73be577f1e923daf96ebee3a65850e51
DW1804 <23521804@gm.uit.edu.vn>
2026-05-08 09:40:16 +0700

    Sửa lỗi chính tả

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def show_menu():
     print("\n===== QUẢN LÝ SÁCH =====")
     print("1. Thêm sách")
-    print("2. Hiển thị danh sách sách")
+    print("2. Hiển thị danh sách")
     print("3. Tìm kiếm sách")
     print("4. Thoát")
