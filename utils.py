1f8b12f2e4b073b7a6289e75f952a7c1cfb9ef2d
ThanhThanh2611 <23521395@gm.uit.edu.vn>
2026-05-08 09:13:48 +0700

    Update menu title and options in show_menu function

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 def show_menu():
-    print("\n===== QUẢN LÝ SÁCH =====")
+    print("\n===== QUẢN LÝ THƯ VIỆN =====")
     print("1. Thêm sách")
     print("2. Hiển thị danh sách sách")
     print("3. Tìm kiếm sách")
-    print("4. Thoát")
+    print("4. Thêm thể loại mới") 
+    print("5. Hiển thị thể loại")  
+    print("6. Thoát")
